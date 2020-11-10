39433306cecefeecd93ee9f1ce7acb941206c2f9
Jae In Song <jaein_song@yonsei.ac.kr>
2020-11-10 12:44:58 +0900

    timegap mod

--- mask_timegap.m
+++ mask_timegap.m
@@ -1,3 +1,6 @@
+gapInMin = 4;
+gap = gapInMin * 60 / 86400 * maxtlen;
+
 % minRefMask = min(min(refmask))
 % maxRefMask = max(max(refmask))
 % for rmi = minRefMask : maxRefMask
@@ -7,16 +10,14 @@
 %     end
 % end
 
-gapInMin = 20;
-gap = gapInMin * 60 / 86400 * maxtlen;
 for hi = 1 : maxhlen
     for ti = 1 : maxtlen-1
-        tRightEnd = min(maxtlen, ti + gap)
+        tRightEnd = min(maxtlen, ti + gap);
         if ~isnan(refmask(hi,ti))&&isnan(refmask(hi,ti+1)) && ~isnan(min(refmask(hi,ti+1:tRightEnd)))
-            for tj = 1 : tRightEnd
-                minRefMask = min(refmask(hi,ti+1:tRightEnd))
+            for tj = ti : tRightEnd
+                minRefMask = min(refmask(hi,ti+1:tRightEnd));
                 if refmask(hi,tj) ~= minRefMask
-                    refmask(refmask=refmask(hi,tj))=minRefMask
+                    refmask(refmask==refmask(hi,tj))=minRefMask;
                 end
             end
         end

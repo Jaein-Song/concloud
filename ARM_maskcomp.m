319369bf752caac7180a600d69b62c2ea36b5221
Jae In Song <jaein_song@Songui-MacBookPro.local>
2020-11-21 19:46:52 +0900

    maskcomp for loop debug

--- ARM_maskcomp.m
+++ ARM_maskcomp.m
@@ -21,7 +21,7 @@ for i=1:fn
 %    load(matfname,'rainmask');
     load(matfname,'validmask');
     for ri=1:3
-        r{ri}.ref2mask=zeros(nh,nt);
+        r{ri,mi}.ref2mask=zeros(nh,nt);
         if  ri==1
             loc = ~isnan(refmask);
         elseif ri==2
@@ -43,82 +43,84 @@ for i=1:fn
                 end
             end
         end
-        r{ri}.ref2mask(loc)=r{ri}.ref2mask(loc)+1;
-        nums(1:nh,validmask>0)=nums(1:nh,validmask>0)+1;
+        r{ri,mi}.ref2mask(loc)=r{ri,mi}.ref2mask(loc)+1;
+        r{ri,mi}.nums(1:nh,validmask>0)=r{ri,mi}.nums(1:nh,validmask>0)+1;
         clear *inst *1m 
     end
     clear refmask velmask validmask
 end
 
-for ri = 1:3
-    r{ri}.ref2mask=r{ri}.ref2mask./nums;
+for mi = 1:mdiv
+    for ri = 1:3
+        r{ri,mi}.ref2mask=r{ri,mi}.ref2mask./r{ri,mi}.nums;
 
-    if mdiv==1
-        refmaskTOT=r{ri}.ref2mask;
-        refmaskTOTLST(:,1:tz_0l)=refmaskTOT(:,tz_0h:nt);
-        refmaskTOTLST(:,tz_0l+1)=refmaskTOT(:,1:tz_0h-1);
-    elseif mdiv==4&&mi==1
-        refmaskDJF=r{ri}.ref2mask;
-        refmaskDJFLST(:,1:tz_0l)=refmaskDJF(:,tz_0h:nt);
-        refmaskDJFLST(:,tz_0l+1:nt)=refmaskDJF(:,1:tz_0h-1);
-    elseif mdiv==4&&mi==2
-        refmaskMAM=r{ri}.ref2mask;
-        refmaskMAMLST(:,1:tz_0l)=refmaskMAM(:,tz_0h:nt);
-        refmaskMAMLST(:,tz_0l+1:nt)=refmaskMAM(:,1:tz_0h-1);
-    elseif mdiv==4&&mi==3
-        refmaskJJA=r{ri}.ref2mask;
-        refmaskJJALST(:,1:tz_0l)=refmaskJJA(:,tz_0h:nt);
-        refmaskJJALST(:,tz_0l+1:nt)=refmaskJJA(:,1:tz_0h-1);
-    elseif mdiv==4&&mi==4
-        refmaskSON=r{ri}.ref2mask;
-        refmaskSONLST(:,1:tz_0l)=refmaskSON(:,tz_0h:nt);
-        refmaskSONLST(:,tz_0l+1:nt)=refmaskSON(:,1:tz_0h-1);
-    end
-    clear nums
-    t24 = [-0.5:1:24.5];
-    h24 = [450:450:13500];
-    h_num = 450/(h(2)-h(1));
-    t_num = 3600/(t(2)-t(1));
-    if mdiv==4
-        for i=1:24
-            for hi=1:length(h24)
-                data=refmaskMAMLST;
-                instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
-                refmaskMAMLST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
-                clear data instmat
-                data=refmaskJJALST;
-                instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
-                refmaskJJALST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
-                clear data instmat
-                data=refmaskSONLST;
-                instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
-                refmaskSONLST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
-                clear data instmat
-                data=refmaskDJFLST;
-                instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
-                refmaskDJFLST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
-                clear data instmat
-            end
+        if mdiv==1
+            refmaskTOT=r{ri,mi}.ref2mask;
+            refmaskTOTLST(:,1:tz_0l)=refmaskTOT(:,tz_0h:nt);
+            refmaskTOTLST(:,tz_0l+1)=refmaskTOT(:,1:tz_0h-1);
+        elseif mdiv==4&&mi==1
+            refmaskDJF=r{ri,mi}.ref2mask;
+            refmaskDJFLST(:,1:tz_0l)=refmaskDJF(:,tz_0h:nt);
+            refmaskDJFLST(:,tz_0l+1:nt)=refmaskDJF(:,1:tz_0h-1);
+        elseif mdiv==4&&mi==2
+            refmaskMAM=r{ri,mi}.ref2mask;
+            refmaskMAMLST(:,1:tz_0l)=refmaskMAM(:,tz_0h:nt);
+            refmaskMAMLST(:,tz_0l+1:nt)=refmaskMAM(:,1:tz_0h-1);
+        elseif mdiv==4&&mi==3
+            refmaskJJA=r{ri,mi}.ref2mask;
+            refmaskJJALST(:,1:tz_0l)=refmaskJJA(:,tz_0h:nt);
+            refmaskJJALST(:,tz_0l+1:nt)=refmaskJJA(:,1:tz_0h-1);
+        elseif mdiv==4&&mi==4
+            refmaskSON=r{ri,mi}.ref2mask;
+            refmaskSONLST(:,1:tz_0l)=refmaskSON(:,tz_0h:nt);
+            refmaskSONLST(:,tz_0l+1:nt)=refmaskSON(:,1:tz_0h-1);
         end
-        refmaskMAMLST_24(:,1)=refmaskMAMLST_24(:,25);
-        refmaskMAMLST_24(:,26)=refmaskMAMLST_24(:,2);
-        refmaskJJALST_24(:,1)=refmaskJJALST_24(:,25);
-        refmaskJJALST_24(:,26)=refmaskJJALST_24(:,2);
-        refmaskSONLST_24(:,1)=refmaskSONLST_24(:,25);
-        refmaskSONLST_24(:,26)=refmaskSONLST_24(:,2);
-        refmaskDJFLST_24(:,1)=refmaskDJFLST_24(:,25);
-        refmaskDJFLST_24(:,26)=refmaskDJFLST_24(:,2);
-    else
-        for i=1:24
-            for hi=1:length(h24)
-                data=refmaskTOTLST;
-                instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
-                refmaskTOTLST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
-                clear data instmat
+        clear nums
+        t24 = [-0.5:1:24.5];
+        h24 = [450:450:13500];
+        h_num = 450/(h(2)-h(1));
+        t_num = 3600/(t(2)-t(1));
+        if mdiv==4
+            for i=1:24
+                for hi=1:length(h24)
+                    data=refmaskMAMLST;
+                    instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
+                    refmaskMAMLST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
+                    clear data instmat
+                    data=refmaskJJALST;
+                    instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
+                    refmaskJJALST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
+                    clear data instmat
+                    data=refmaskSONLST;
+                    instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
+                    refmaskSONLST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
+                    clear data instmat
+                    data=refmaskDJFLST;
+                    instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
+                    refmaskDJFLST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
+                    clear data instmat
+                end
             end
+            refmaskMAMLST_24(:,1)=refmaskMAMLST_24(:,25);
+            refmaskMAMLST_24(:,26)=refmaskMAMLST_24(:,2);
+            refmaskJJALST_24(:,1)=refmaskJJALST_24(:,25);
+            refmaskJJALST_24(:,26)=refmaskJJALST_24(:,2);
+            refmaskSONLST_24(:,1)=refmaskSONLST_24(:,25);
+            refmaskSONLST_24(:,26)=refmaskSONLST_24(:,2);
+            refmaskDJFLST_24(:,1)=refmaskDJFLST_24(:,25);
+            refmaskDJFLST_24(:,26)=refmaskDJFLST_24(:,2);
+        else
+            for i=1:24
+                for hi=1:length(h24)
+                    data=refmaskTOTLST;
+                    instmat=data((hi-1)*h_num+1:hi*h_num,(i-1)*t_num+1:i*t_num);
+                    refmaskTOTLST_24(hi,i+1)=mean(instmat(~isnan(instmat)));
+                    clear data instmat
+                end
+            end
+            refmaskTOTLST_24(:,1)=refmaskTOTLST_24(:,25);
+            refmaskTOTLST_24(:,26)=refmaskTOTLST_24(:,2);
         end
-        refmaskTOTLST_24(:,1)=refmaskTOTLST_24(:,25);
-        refmaskTOTLST_24(:,26)=refmaskTOTLST_24(:,2);
+        comp_outfile
     end
-    comp_outfile
 end
